347f48adc405a98c40580943ced76230d37919a6
zirujauak <evan.day@gmail.com>
2018-12-08 11:36:21 -0800

    Util functions

--- src/advent_of_code_2018/util.clj
+++ src/advent_of_code_2018/util.clj
@@ -1,8 +1,22 @@
 (ns advent-of-code-2018.util
   (:require [clojure.java.io :as io]))
 
-(defn read-file-lines
-  [filename]
-  (->> (io/resource filename)
-       io/reader
-       line-seq))
+(defn input-filename
+  [day]
+  (str "day-" day "-input.txt"))
+
+(defn read-file
+  [day]
+  (-> day
+      input-filename
+      io/resource
+      io/reader
+      slurp))
+
+(defn read-file-as-lines
+  [day]
+  (-> day
+      input-filename
+      io/resource
+      io/reader
+      line-seq))
